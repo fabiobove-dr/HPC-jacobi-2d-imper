2fc6da357868fad5d26fbaddaaaaf1b0261a9a78
Fabio Bove <fabio.bove.dr@gmail.com>
2025-02-03 16:15:13 +0100

    UPDATE: added a few line (optimization notes)

--- cuda-code/jacobi-2d-imper.cuh
+++ cuda-code/jacobi-2d-imper.cuh
@@ -3,7 +3,7 @@
 
 /* Default to STANDARD_DATASET. */
 # if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET) && !defined(MAX_DATASET)
-#  define EXTRALARGE_DATASET
+#  define SMALL_DATASET
 # endif
 
 /* Do not define anything if the user manually defines the size. */
